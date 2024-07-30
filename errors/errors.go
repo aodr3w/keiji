3f01a2945a9613facebf1d4997f7ca27c24e961a
AndrewOdiit <andrewodiit2020@gmail.com>
2024-07-30 20:43:53 +0300

    update errors

--- errors/errors.go
+++ errors/errors.go
@@ -1,19 +1,55 @@
 package cmdErrors
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type PathNotFound struct {
+	Message string
+}
+
+func (e *PathNotFound) Error() string {
+	return e.Message
+}
+func NewPathNotFound(path string) *PathNotFound {
+	return &PathNotFound{
+		fmt.Sprintf("path %v not found", path),
+	}
+}
+func Is(target error) bool {
+	_, ok := target.(*PathNotFound)
+	return ok
+}
+
+type ServiceNotFound struct {
+	Message string
+}
+
+func (e *ServiceNotFound) Error() string {
+	return e.Message
+}
+func NewServiceNotFound() *ServiceNotFound {
+	return &ServiceNotFound{
+		"service not found",
+	}
+}
+
+func (*ServiceNotFound) Is(target error) bool {
+	_, ok := target.(*ServiceNotFound)
+	return ok
+}
 
 // PIDNotFound is a custom error type for PID file not found errors.
 type PIDNotFound struct {
 	Message string
 }
 
-// Implement the Error() method from the error interface.
 func (e *PIDNotFound) Error() string {
 	return e.Message
 }
 
 // NewPIDNotFound creates a new PIDNotFound error with the given message.
-func NewPIDNotFound() error {
+func NewPIDNotFound() *PIDNotFound {
 	return &PIDNotFound{Message: "PID file not found"}
 }
 
@@ -38,6 +74,12 @@ func (w *WorkSpaceInitError) Is(target error) bool {
 	return ok
 }
 
+var ErrPathNotFound = func(path string) *PathNotFound {
+	return NewPathNotFound(
+		path,
+	)
+}
+var ErrServiceNotFound = NewServiceNotFound()
 var ErrPIDNotFound = NewPIDNotFound()
 var ErrWorkSpaceInit = func(msg string, args ...interface{}) *WorkSpaceInitError {
 	return NewWorkSpaceInitError(
